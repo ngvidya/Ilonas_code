fec21b000517f3e98a684439b37c2cd1299582cc
ngvidya <ngvidya2021@gmail.com>
2021-04-24 11:57:39 -0400

    Illonas Code

---
+++ todo-list-main/src/app/service/storage.service.ts
@@ -0,0 +1,20 @@
+import { JsonPipe } from '@angular/common';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  getData(key: string): any {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  setData(key: string, data: any): void {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
+
+  constructor() { }
+
+
+}
